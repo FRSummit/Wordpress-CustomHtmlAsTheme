0b94f8f57840331d974deb4b77ca6443f332c809
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-10-19 13:16:51 +0600

    pagination added

--- wp-content/themes/frs-theme/archive.php
+++ wp-content/themes/frs-theme/archive.php
@@ -22,6 +22,18 @@
                                                 <?php the_excerpt();?>
                                             </div>
                                         <?php endwhile; else: endif;?>
+
+                                        <?php
+                                            global $wp_query;
+                                            $big = 999999999;
+                                            echo paginate_links(array(
+                                                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big)
+                                            )),
+                                                'format' => '?paged=%#%',
+                                                'current' => max(1, get_query_var('paged')),
+                                                'total' => $wp_query->max_num_pages
+                                            ));
+                                        ?>
                                 </div>
                         </div>
 
