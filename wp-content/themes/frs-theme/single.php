8bb3495443aa80c07c36bb900c63d0ebc41d92e4
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-10-19 13:04:55 +0600

    Blog tags added

--- wp-content/themes/frs-theme/single.php
+++ wp-content/themes/frs-theme/single.php
@@ -18,6 +18,8 @@
                                         <?php if(have_posts()) : while(have_posts()) : the_post();?>
                                                 <?php the_content();?>
                                         <?php endwhile; else: endif;?>
+
+                                        <?php the_tags();?>
                                 </div>
                         </div>
 
