8d6a7de0b11ca4c219ea1b5df0eb3f4a7b51f6df
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-10-19 10:52:10 +0600

    404 page added

---
+++ wp-content/themes/frs-theme/404.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+
+<section>
+    <div class="content">
+        <div class="container">
+            <h1>This page was not found</h1>
+        </div>
+    </div>
+</section>
+
+<?php get_footer();?>
